b6532f6620d5d2aab8aeee5e509a426439cbb8e5
Ra <tryampampam@mail.ru>
2022-02-16 22:31:01 +0300

    Доработка проекта

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 //alert('Hello from my extension!');
-const url=window.location.href,
-      urlServer = 'http://127.0.0.1:4777/ch_fc/set_url',
-      data = { url: url };
-//console.log(url);
-const request = new Request(urlServer,{
-  method: 'POST', // или 'PUT'
-  body: JSON.stringify(data), // данные могут быть 'строкой' или {объектом}!
-  headers: {
-    'Content-Type': 'application/json'
-  }
-});
-try {
-  fetch(request)
-    .then((response) => {
-      response.json().then((response2) => {
-        console.log(response2);
+const saveUrl=()=>{
+  const url=window.location.href,
+        urlServer = 'http://127.0.0.1:4777/ch_fc/set_url',
+        data = { url: url };
+  //console.log(url);
+  const request = new Request(urlServer,{
+    method: 'POST', // или 'PUT'
+    body: JSON.stringify(data), // данные могут быть 'строкой' или {объектом}!
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  try {
+    fetch(request)
+      .then((response) => {
+        response.json().then((response2) => {
+          console.log(response2);
+        });
+      })
+      .then((data) => {
+        console.log(data);
       });
-    })
-    .then((data) => {
-      console.log(data);
-    });
-} catch (error) {
-  console.error('Ошибка:', error);
-}
+  } catch (error) {
+    console.error('Ошибка:', error);
+  }
+};
+saveUrl();
+window.onload = function() {
+    window.onfocus = function() {
+        saveUrl();
+    };
+};
